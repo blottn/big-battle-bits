30565d0c583f4d741048baa2b11fd5b43d1c4709
Nicholas Blott <blottn@tcd.ie>
2021-06-30 05:30:46 +0100

    Add reading back data

--- bf/bf.go
+++ bf/bf.go
@@ -13,10 +13,10 @@ import (
 )
 
 type BattleGround struct {
-	width  int
-	height int
-	nm     noisemap.NoiseMap
-	armies map[image.Point]Force
+	width   int
+	height  int
+	terrain image.Image
+	armies  Armies
 }
 
 var Ocean = color.RGBA{0, 68, 128, 255}
@@ -28,15 +28,6 @@ func centerDist(x, y, width, height int) float64 {
 	return math.Sqrt(xDif*xDif + yDif*yDif)
 }
 
-func (bg BattleGround) terrainColor(x, y int) color.Color {
-	dist := centerDist(x, y, bg.width, bg.height)
-	n := bg.nm[image.Point{x, y}]
-	if n > dist*0.8+0.3 { // Please don't touch these magic numbers
-		return Land
-	}
-	return nil
-}
-
 func (bg BattleGround) forceColor(x, y int) color.Color {
 	for point, force := range bg.armies {
 		if point.X == x && point.Y == y {
@@ -46,13 +37,81 @@ func (bg BattleGround) forceColor(x, y int) color.Color {
 	return nil
 }
 
+func getTerrain(x, y, w, h int, nm noisemap.NoiseMap) color.Color {
+	dist := centerDist(x, y, w, h)
+	n := nm[image.Point{x, y}]
+	if n > dist*0.8+0.3 {
+		return Land
+	}
+	return Ocean
+}
+
+func newTerrain(width, height int, nm noisemap.NoiseMap) image.Image {
+	img := image.NewRGBA(image.Rect(0, 0, width, height))
+	for i := 0; i < width; i++ {
+		for j := 0; j < height; j++ {
+			img.Set(i, j, getTerrain(i, j, width, height, nm))
+		}
+	}
+	return img
+}
+
 func NewBattleGround(width, height, seed int) BattleGround {
+	nm := noisemap.NewNoiseMap(width, height, noise.NewPerlin(1, 2, 3, int64(seed)))
 	return BattleGround{
 		width,
 		height,
-		noisemap.NewNoiseMap(width, height, noise.NewPerlin(1, 2, 3, int64(seed))),
+		newTerrain(width, height, nm),
 		nil,
 	}
+}
+
+func compareColor(left, right color.Color) bool {
+	left_r, left_g, left_b, left_a := left.RGBA()
+	right_r, right_g, right_b, right_a := right.RGBA()
+	return left_r == right_r &&
+		left_g == right_g &&
+		left_b == right_b &&
+		left_a == right_a
+}
+
+func From(reader io.Reader) (*BattleGround, error) {
+	// Collect armies
+	img, err := png.Decode(reader)
+	if err != nil {
+		return nil, err
+	}
+	bounds := img.Bounds()
+	width := bounds.Max.X
+	height := bounds.Max.Y
+
+	terrain := image.NewRGBA(bounds)
+	forces := Armies{}
+	for i := 0; i < width; i++ {
+		for j := 0; j < height; j++ {
+			if compareColor(Ocean, img.At(i, j)) {
+				terrain.Set(i, j, Ocean)
+			} else if compareColor(Land, img.At(i, j)) {
+				terrain.Set(i, j, Land)
+			} else {
+				col := img.At(i, j)
+				points, ok := armies[col]
+				if !ok {
+					armies[col] = []image.Point{}
+				}
+				armies[col] = append(armies[col], image.Point(i, j))
+
+			}
+		}
+	}
+
+	return &BattleGround{
+		width,
+		height,
+		terrain,
+		armies,
+	}, nil
+
 }
 
 func (bg BattleGround) Output(writer io.Writer) error {
@@ -73,9 +132,5 @@ func (bg BattleGround) At(x, y int) color.Color {
 		return fc
 	}
 
-	if tc := bg.terrainColor(x, y); tc != nil {
-		return tc
-	}
-
-	return Ocean
+	return bg.terrain.At(x, y)
 }
---
+++ cmd/manip.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"bbb/bf"
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	bfFile := flag.String("bf-file", "bf-png", "The loction for the initial Battlefield")
+	flag.Parse()
+	file, err := os.Open(*bfFile)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	defer file.Close()
+
+	bf, err := bf.From(file)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	out, _ := os.OpenFile("out.png", os.O_RDWR|os.O_CREATE, 0755)
+	defer out.Close()
+	bf.Output(out)
+}
