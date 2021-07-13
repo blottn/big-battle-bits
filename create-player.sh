2e63694428821cb87555980f45585861e9ea9621
Nicholas Blott <blottn@tcd.ie>
2021-07-13 14:15:20 +0100

    Added script to create sample player

---
+++ create-player.sh
@@ -0,0 +1,1 @@
+curl -XPOST localhost:8080/playerConfigs/862322123539087380/1234 -d'{"priority":1.0, "start": {"x":50,"y":50}, "color": {"R":255,"G":0,"B":0,"A":255}}'
--- game/player.go
+++ game/player.go
@@ -11,13 +11,13 @@ type PlayerConfigs map[string]PlayerConfig
 type PlayerConfig struct {
 	Priority bf.Angle
 	Start    image.Point
-	Color    color.Color
+	Color    color.RGBA
 }
 
 func NewDefaultPlayerConfig() PlayerConfig {
 	return PlayerConfig{
 		bf.Angle(0),
 		image.Point{0, 0},
-		color.Black,
+		bf.Black,
 	}
 }
